87c44f72487753b818891ab9697398166b485dda
AlexHartford <hartforda@msoe.edu>
2017-10-18 12:27:33 -0500

    Added sharpe ratio calculation

--- src/Analyzer/AnalyzerController.java
+++ src/Analyzer/AnalyzerController.java
@@ -29,7 +29,14 @@ public class AnalyzerController {
 
     private Date startDate, endDate;
 
+    private boolean stockValuesNeedCalculating;
+    private boolean portfolioValueNeedsCalculating;
+    private boolean portfolioReturnNeedsCalculating;
+    private boolean spyValueNeedsCalculating;
+
     private double portfolioValue = 1.00;
+
+    private int numRows = 250;
 
     private ArrayList<String> stockSymbols;
     private String CELG = "CELG";
@@ -37,25 +44,18 @@ public class AnalyzerController {
     private String GOOG = "GOOG";
     private String NVDA = "NVDA";
 
+    private String SPY = "SPY";
+
     private ArrayList<Double> portfolioAllocations;
     private double CELGweight = 0.3;
     private double FBweight = 0.3;
     private double GOOGweight = 0.2;
     private double NVDAweight = 0.2;
 
+    private double SPYweight = 1.0;
+
     public void initialize() {
         setUp();
-    }
-
-    /**
-     * Calculate the Sharpe ratio
-     */
-    private void sharpen() {
-//        E[Ra - Rb] / sqroot(var[Ra-Rb])
-        float assetReturn;
-        float benchmarkAssetReturn;
-        float mean;
-        float stdDev;
     }
 
     /**
@@ -63,29 +63,12 @@ public class AnalyzerController {
      */
     @FXML
     private void getData() {
-        if (parseDates()) {
 
-            ResultSet results = null;
-            Statement statement = null;
-            String query = "select * from portfolio";
-            try {
-                statement = connection.createStatement();
-                results = statement.executeQuery(query);
-                ResultSetMetaData resultSetMetaData = (ResultSetMetaData) results
-                        .getMetaData();
+        calculateStockValues();
+        calculatePortfolioValue();
+        calculatePortfolioReturn();
+        sharpen();
 
-//                while (results.next()) {
-//                    System.out.print(results.getDate("date"));
-//                    System.out.print(" | ");
-//                    System.out.println(results.getFloat("CELGcumulativeReturn"));
-//                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            errorMsg.setText("");
-        } else {
-            errorMsg.setText("Invalid Start or End Date!");
-        }
     }
 
     /**
@@ -116,16 +99,20 @@ public class AnalyzerController {
             } catch (ParseException e) {
                 e.printStackTrace();
             }
+            errorMsg.setText("");
             return true;
         }
+        errorMsg.setText("Invalid Start or End Date!");
         return false;
     }
 
     /**
      * Calculates the value for each stock and updates the portfolio accordingly.
+     * Only need to run this once each time you adjust your portfolio.
+     * TODO: Add radio button or toggle switch to GUI
      */
     private void calculateStockValues() {
-        if (parseDates()) {
+        if (parseDates() && stockValuesNeedCalculating) {
             for (int i = 0; i < stockSymbols.size(); i++) {
                 String updateQuery = "UPDATE portfolio SET "
                         + stockSymbols.get(i) + "value = "
@@ -138,6 +125,112 @@ public class AnalyzerController {
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
+            }
+        }
+    }
+
+    private double calculateAverage() {
+
+        double average = 0;
+        if (parseDates()) {
+
+            ResultSet results = null;
+            Statement statement = null;
+            String query = "select * from portfolio WHERE Date >= '" + startDate + "' AND Date <= '" + endDate + "'";
+            try {
+                statement = connection.createStatement();
+                results = statement.executeQuery(query);
+                ResultSetMetaData resultSetMetaData = (ResultSetMetaData) results
+                        .getMetaData();
+
+                while (results.next()) {
+                    average += (results.getFloat("PortfolioCumulativeReturn") - results.getFloat("SPYcumulativeReturn")) / numRows;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return average;
+    }
+
+    private double calculateStandardDeviation(double average) {
+        double standardDeviation = 0;
+        double sum = 0;
+
+        if (parseDates()) {
+
+            ResultSet results = null;
+            Statement statement = null;
+            String query = "select * from portfolio WHERE Date >= '" + startDate + "' AND Date <= '" + endDate + "'";
+            try {
+                statement = connection.createStatement();
+                results = statement.executeQuery(query);
+                ResultSetMetaData resultSetMetaData = (ResultSetMetaData) results
+                        .getMetaData();
+
+                while (results.next()) {
+                    sum += Math.pow(Math.abs((results.getFloat("PortfolioCumulativeReturn") - results.getFloat("SPYcumulativeReturn")) - average), 2);
+                }
+                standardDeviation = Math.sqrt(sum / numRows);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return standardDeviation;
+    }
+
+    /**
+     * Calculate the Sharpe ratio
+     */
+    private void sharpen() {
+        double average = calculateAverage();
+        double standardDeviation = calculateStandardDeviation(average);
+        double sharpe = Math.sqrt(numRows) * average / standardDeviation;
+        sharpeLabel.setText(String.valueOf(sharpe));
+        dailyAvgLabel.setText(String.valueOf(average));
+        dailyStdDevLabel.setText(String.valueOf(standardDeviation));
+    }
+
+    /**
+     * Calculates the portfolio's value by day
+     * Only need to run this once each time you adjust your portfolio.
+     * TODO: Add radio button or toggle switch to GUI
+     */
+    private void calculatePortfolioValue() {
+        if (parseDates() && portfolioValueNeedsCalculating) {
+
+            String updateQuery = "UPDATE portfolio SET portfolioValue = "
+                    + CELG + "value + " + FB + "value + " + GOOG + "value + " + NVDA + "value"
+                    + " WHERE Date >= '" + startDate + "' AND Date <= '" + endDate + "'";
+
+            Statement statement = null;
+            try {
+                statement = connection.createStatement();
+                statement.executeUpdate(updateQuery);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Calculates the portfolio's return by day
+     * Only need to run this once each time you adjust your portfolio.
+     * TODO: Add radio button or toggle switch to GUI
+     */
+    private void calculatePortfolioReturn() {
+        if (parseDates() && portfolioReturnNeedsCalculating) {
+
+            String updateQuery = "UPDATE portfolio SET portfolioCumulativeReturn = "
+                    + "portfolioValue / " + portfolioValue
+                    + " WHERE Date >= '" + startDate + "' AND Date <= '" + endDate + "'";
+
+            Statement statement = null;
+            try {
+                statement = connection.createStatement();
+                statement.executeUpdate(updateQuery);
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         }
     }
@@ -157,6 +250,17 @@ public class AnalyzerController {
         portfolioAllocations.add(FBweight);
         portfolioAllocations.add(GOOGweight);
         portfolioAllocations.add(NVDAweight);
+
+        stockValuesNeedCalculating = false;
+        portfolioValueNeedsCalculating = false;
+        portfolioReturnNeedsCalculating = false;
+        spyValueNeedsCalculating = false;
+
+        if (spyValueNeedsCalculating) {
+            stockSymbols.add(SPY);
+            portfolioAllocations.add(SPYweight);
+        }
+
     }
 
     /**
---
+++ src/Settings/SettingsController.java
@@ -0,0 +1,7 @@
+package Settings;
+
+/**
+ * Created by hartforda on 10/17/2017.
+ */
+public class SettingsController {
+}
